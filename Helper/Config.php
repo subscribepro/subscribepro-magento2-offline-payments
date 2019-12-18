f8096a92793789db2ad65f302be078658b3790f5
David King <imkingdavid@gmail.com>
2019-12-17 23:07:55 -0500

    Fixed a bug that caused the 'default' scope value to override the 'website' scope configuration for allowed offline payment methods

--- Helper/Config.php
+++ Helper/Config.php
@@ -4,18 +4,18 @@ namespace SubscribePro\OfflinePayments\Helper;
 
 use Magento\Framework\App\Helper\Context;
 use Magento\Framework\App\Helper\AbstractHelper;
-use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
 
 
 class Config extends AbstractHelper
 {
-    public function getAllowedPaymentMethods($scope = ScopeConfigInterface::SCOPE_TYPE_DEFAULT)
+    public function getAllowedPaymentMethods($scope = ScopeInterface::SCOPE_WEBSITE)
     {
         $paymentMethodsString = $this->getAllowedPaymentMethodsString($scope);
         return !empty($paymentMethodsString) ? explode(',', $paymentMethodsString) : [];
     }
 
-    public function getAllowedPaymentMethodsString($scope = ScopeConfigInterface::SCOPE_TYPE_DEFAULT)
+    public function getAllowedPaymentMethodsString($scope = ScopeInterface::SCOPE_WEBSITE)
     {
         return $this->scopeConfig->getValue(
             'swarming_subscribepro/advanced/offline_payment_methods',
